c2329d410aef58e766b00f8c389507bf27f8ed50
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-07-07 08:13:31 -0700

    initial commit

--- src/main/java/com/mmorrell/ArcanaApplication.java
+++ src/main/java/com/mmorrell/ArcanaApplication.java
@@ -1,10 +1,11 @@
 package com.mmorrell;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArcanaMain {
+public class ArcanaApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ArcanaApplication.class, args);
     }
 }
