afed790efceadf0bacf9b8a6f9e66658d6baf8b6
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-07-08 14:23:05 -0700

    Add strategy interface note

--- src/main/java/com/mmorrell/arcana/strategies/Strategy.java
+++ src/main/java/com/mmorrell/arcana/strategies/Strategy.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class Strategy {
+    // TODO: provide to superclasses: rpcClient, backgroundCache, privateKey
+    // so they don't have to implement these themselves
 
     public abstract void start();
 
